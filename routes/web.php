a0fcb7e33df6483984131da9355d7a4f1eb68d50
federicaarosio <arosio.fede@gmail.com>
2024-06-05 19:18:58 +0200

    added admin and instructor middleware

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,12 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+// Admin Group Middleware
+Route::middleware(['auth', 'roles:admin'])->group(function(){
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
 
-Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+// Instructor Group Middleware
+Route::middleware(['auth', 'roles:instructor'])->group(function(){
+    Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+});
